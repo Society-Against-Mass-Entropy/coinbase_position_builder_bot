3922331b14cf76f6ccd374e6de2a26c2827f5f44
[._.]/ Adam Eivy <70015+atomantic@users.noreply.github.com>
2021-02-21 21:17:17 -0800

    REBUY_AT is now a list of percentage drop targets (so we do not just double it every time)

--- run.btcusd.limit_only.config.js
+++ run.btcusd.limit_only.config.js
@@ -1,3 +1,7 @@
+/**
+ * sample config that runs BTC-USD every 2 hours but only with posting LIMIT orders
+ * NO regular engine MAKER orders are performed
+ */
 const apiKeys = require("./api.keys");
 module.exports = {
   apps: [
@@ -14,13 +18,19 @@ module.exports = {
         CPBB_FREQ: "3 */2 * * *",
         CPBB_TICKER: "BTC",
         CPBB_CURRENCY: "USD",
-        CPBB_VOL: 10,
-        CPBB_APY: 5, // sell if over 5% APY
+        CPBB_VOL: 10, // $10
+        CPBB_APY: 10, // sell if over 10% APY
+        // should the engine only create and manage the limit orders and not make normal accumulation trades
+        // useful for testing this feature
+        // or for running a bot that only wants to accumulate an asset via dips
+        CPBB_REBUY_ONLY: true,
         // place up to 5 maker limit orders on the books after a sell action
         CPBB_REBUY_MAX: 5,
         // maximum dollar value consumed by limit order placements
         CPBB_REBUY_VOL: 10,
-        // minimum order is in BTC (.0001, which is $5 at $50K, or $2 at $20K)
+        // minimum order is in BTC (.0001, which is $5 at $50K)
+        // minimum order is in ETH (.001, which is $5 at $5K)
+        // minimum order is in LTC (.01, which is $5 at $500)
         // rebuy logic will place up to CPBB_REBUY_MAX orders at this size until CPBB_REBUY_VOL is reached
         CPBB_REBUY_SIZE: .0001,
         // should the size be doubled (or otherwise multiplied) at each percentage drop (.e.g .0001, .0002)?
