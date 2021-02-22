29b8ec241c52e909f5d5d889704481dc95b39f84
[._.]/ Adam Eivy <70015+atomantic@users.noreply.github.com>
2021-02-21 21:33:31 -0800

    better docs in run samples

--- run.btcusd.limit_only.config.js
+++ run.btcusd.limit_only.config.js
@@ -15,11 +15,12 @@ module.exports = {
         CPBB_APIKEY: apiKeys.CPBB_APIKEY,
         CPBB_APISEC: apiKeys.CPBB_APISEC,
         // VERBOSE: true,
-        CPBB_FREQ: "3 */2 * * *",
+        // run at 5:03 am daily
+        CPBB_FREQ: "3 5 * * *",
         CPBB_TICKER: "BTC",
         CPBB_CURRENCY: "USD",
-        CPBB_VOL: 10, // $10
-        CPBB_APY: 10, // sell if over 10% APY
+        CPBB_VOL: 50, // $50 each day
+        CPBB_APY: 100, // sell if over 100% APY
         // should the engine only create and manage the limit orders and not make normal accumulation trades
         // useful for testing this feature
         // or for running a bot that only wants to accumulate an asset via dips
@@ -27,7 +28,7 @@ module.exports = {
         // place up to 5 maker limit orders on the books after a sell action
         CPBB_REBUY_MAX: 5,
         // maximum dollar value consumed by limit order placements
-        CPBB_REBUY_VOL: 10,
+        CPBB_REBUY_VOL: 50,
         // minimum order is in BTC (.0001, which is $5 at $50K)
         // minimum order is in ETH (.001, which is $5 at $5K)
         // minimum order is in LTC (.01, which is $5 at $500)
@@ -35,10 +36,10 @@ module.exports = {
         CPBB_REBUY_SIZE: .0001,
         // should the size be doubled (or otherwise multiplied) at each percentage drop (.e.g .0001, .0002)?
         // acts as a multiplier if you want to just go to .00015 with 1.5
-        CPBB_REBUY_MULTIPLIER: 2,
+        CPBB_REBUY_MULTIPLIER: 1.5,
         // rebuy at these percentage drop targets (-1%, -2%, etc)
         // note: you have to define at least the number of points in CPBB_REBUY_MAX
-        CPBB_REBUY_AT: "-1,-2,-4,-8,-16",
+        CPBB_REBUY_AT: "-1,-2,-4,-8,-16,-20,-30,-40,-50,-80",
       },
     },
   ],
--- run.btcusd.rebuy.config.js
+++ run.btcusd.rebuy.config.js
@@ -15,20 +15,30 @@ module.exports = {
         CPBB_TICKER: "BTC",
         CPBB_CURRENCY: "USD",
         CPBB_VOL: 10,
-        CPBB_APY: 5, // sell if over 5% APY
-        // place up to 5 maker limit orders on the books after a sell action
-        CPBB_REBUY_MAX: 5,
+        CPBB_APY: 10, // sell if over 10% APY
+        // MAKER REBUY CONFIG
+        // The below section will create the following maker orders after a sell
+        // .0001 BTC @ price -1% (as long as bitcoin is under $100K)
+        // .0002 BTC @ price -2% (but only if this doesn't bust the $10 max)
+        // .0004 BTC @ price -4% (but only if this doesn't bust the $10 max)
+        // .0008 BTC @ price -8% (but only if this doesn't bust the $10 max)
+        // .0016 BTC @ price -16% (but only if this doesn't bust the $10 max)
+
+        // rebuy logic will place up to CPBB_REBUY_MAX orders
+        // at this size (multiplied by CPBB_REBUY_MULTIPLIER) until CPBB_REBUY_VOL is reached
+        // using CPBB_REBUY_AT as each drop point
+        // place up to 10 maker limit orders on the books after a sell action
+        CPBB_REBUY_MAX: 10,
         // maximum dollar value consumed by limit order placements
         CPBB_REBUY_VOL: 10,
-        // minimum order is in BTC (.0001, which is $5 at $50K, or $2 at $20K)
-        // rebuy logic will place up to CPBB_REBUY_MAX orders at this size until CPBB_REBUY_VOL is reached
+        // minimum order is in BTC (.0001, which is $5 at $50K)
         CPBB_REBUY_SIZE: .0001,
-        // should the size be doubled (or otherwise multiplied) at each percentage drop (.e.g .0001, .0002)?
-        // acts as a multiplier if you want to just go to .00015 with 1.5
-        CPBB_REBUY_MULTIPLIER: 2,
+        // what should the size be multiplied by each drop point (1 means keep it stable),
+        // 2 would set an order of .0001, then .0002, then .0004, etc
+        CPBB_REBUY_MULTIPLIER: 1,
         // rebuy at these percentage drop targets (-1%, -2%, etc)
         // note: you have to define at least the number of points in CPBB_REBUY_MAX
-        CPBB_REBUY_AT: "-1,-2,-4,-8,-16",
+        CPBB_REBUY_AT: "-1,-2,-4,-8,-16,-20,-30,-40,-50,-80",
       },
     },
   ],
