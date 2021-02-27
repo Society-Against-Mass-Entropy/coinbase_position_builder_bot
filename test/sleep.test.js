fc09a0dbcbbe89a93d82e50bf145d312261a4498
Jason Edison <jason.x.edison@gmail.com>
2021-02-27 13:00:16 -0800

    learning how to write unit tests with some simple libraries that do not make API calls

---
+++ test/sleep.test.js
@@ -0,0 +1,18 @@
+const sleep = require("../lib/sleep");
+
+test("sleep 0", async () => {
+  const startTime = new Date().getTime();
+  await sleep();
+  const endTime = new Date().getTime();
+  const elapsed = endTime - startTime;
+  expect(elapsed).toBeGreaterThan(0);
+  expect(elapsed).toBeLessThan(10);
+});
+test("sleep 200ms", async () => {
+  const startTime = new Date().getTime();
+  await sleep(200);
+  const endTime = new Date().getTime();
+  const elapsed = endTime - startTime;
+  expect(elapsed).toBeGreaterThan(200);
+  expect(elapsed).toBeLessThan(500);
+});
