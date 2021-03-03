04fb4e1797e617d19e929fe380df39a5141a73e4
[._.]/ Adam Eivy <70015+atomantic@users.noreply.github.com>
2021-03-02 20:55:09 -0800

    no longer need extra timeout on jest

--- jest.config.js
+++ jest.config.js
@@ -165,7 +165,7 @@ module.exports = {
   // This option allows use of a custom test runner
   // testRunner: "jasmine2",
 
-  testTimeout: 120000, // 2 minutes
+  // testTimeout: 120000, // 2 minutes
 
   // This option sets the URL for the jsdom environment. It is reflected in properties such as location.href
   // testURL: "http://localhost",
