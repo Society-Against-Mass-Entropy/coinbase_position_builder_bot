03a3cbfdd5eb4f421017616993da7f7dfbd3581e
Jason Edison <jason.x.edison@gmail.com>
2021-03-04 19:05:43 -0800

    correct rounding error on printing drop target with limit order status (just in log)

--- run.mine.config.js
+++ run.mine.config.js
@@ -20,7 +20,7 @@ module.exports = {
         CPBB_VOL: 100,
         CPBB_APY: 150,
         // max $ spend on limit rebuys
-        CPBB_REBUY_MAX: 50,
+        CPBB_REBUY_MAX: 75,
         // minimum order is .0001 BTC ($5 at $50K)
         // rebuy logic will place up orders at this size until CPBB_REBUY_MAX is reached
         // CPBB_REBUY_SIZE:
@@ -30,7 +30,7 @@ module.exports = {
         // CPBB_REBUY_AT:
         //   '-4,-6,-8,-10,-12,-15,-20,-25,-30,-35,-40,-50,-60,-70,-80,-90',
         CPBB_REBUY:
-          '.0001@4,.0002@6,.0004@8,.0008@10,.001@12,.002@15,.004@20,.008@25,.016@30,.032@35,.064@40,.128@50,.256@60,.512@70,1.024@80,2.048@90',
+          '.0001@2,.0002@3,.0003@4,.0004@5,.0005@6,.0006@7,.0007@8,.0008@10,.001@12,.002@15,.004@20,.008@25,.016@30,.032@35,.064@40,.128@50,.256@60,.512@70,1.024@80,2.048@90',
         // when should we cancel limit orders?
         // default behavior is on the next action point (if they didn't fill)
         // if CPBB_REBUY_CANCEL is set, this is a number of minutes after the limit order
@@ -59,14 +59,14 @@ module.exports = {
         CPBB_FREQ: '15 */4 * * *',
         CPBB_TICKER: 'ETH',
         CPBB_CURRENCY: 'USD',
-        CPBB_VOL: 5,
+        CPBB_VOL: 10,
         CPBB_APY: 100,
         // max $ spend on limit rebuys
-        CPBB_REBUY_MAX: 5,
+        CPBB_REBUY_MAX: 10,
         // minimum order is .001 ETH ($5 at $5K)
         // rebuy logic will place orders at this size until CPBB_REBUY_MAX is reached
         CPBB_REBUY:
-          '.001@4,.002@6,.003@8,.004@10,.005@12,.01@15,.02@20,.04@25,.08@30,.16@35,.32@40,.64@50,1.28@60,2.56@70,5.12@80,10.24@90',
+          '.001@2,.002@4,.003@6,.004@8,.005@10,.01@15,.02@20,.04@25,.08@30,.16@35,.32@40,.64@50,1.28@60,2.56@70,5.12@80,10.24@90',
         CPBB_REBUY_CANCEL: 60 * 24 * 14,
         CPBB_REBUY_REBUILD: 17,
       },
@@ -80,17 +80,17 @@ module.exports = {
         CPBB_APIPASS: apiKeys.CPBB_APIPASS,
         CPBB_APIKEY: apiKeys.CPBB_APIKEY,
         CPBB_APISEC: apiKeys.CPBB_APISEC,
-        CPBB_FREQ: '25 8 * * *',
+        CPBB_FREQ: '25 */4 * * *',
         CPBB_TICKER: 'LTC',
         CPBB_CURRENCY: 'USD',
-        CPBB_VOL: 25,
+        CPBB_VOL: 10,
         CPBB_APY: 100,
         // max $ spend on limit rebuys
-        CPBB_REBUY_MAX: 15,
+        CPBB_REBUY_MAX: 10,
         // minimum order is in LTC (.01, which is $5 at $500)
         // rebuy logic will place orders at this size until CPBB_REBUY_MAX is reached
         CPBB_REBUY:
-          '.01@4,.02@6,.03@8,.04@10,.05@12,.1@15,.2@20,.4@25,.8@30,1.6@35,3.2@40,6.4@50,12.8@60,25.6@70,51.2@80,102.4@90',
+          '.01@2,.02@4,.03@6,.04@8,.05@10,.1@15,.2@20,.4@25,.8@30,1.6@35,3.2@40,6.4@50,12.8@60,25.6@70,51.2@80,102.4@90',
         CPBB_REBUY_CANCEL: 60 * 24 * 14,
         CPBB_REBUY_REBUILD: 17,
       },
@@ -117,7 +117,7 @@ module.exports = {
           '.01,.02,.03,.04,.05,.1,.2,.4,.8,1.6,3.2,6.4,12.8,25.6,51.2,102.4',
         // note: you have to define at least the number of points in CPBB_REBUY_SIZE
         CPBB_REBUY_AT:
-          '-4,-6,-8,-10,-12,-15,-20,-25,-30,-35,-40,-50,-60,-70,-80,-90',
+          '-2,-4,-6,-8,-10,-15,-20,-25,-30,-35,-40,-50,-60,-70,-80,-90',
         CPBB_REBUY_CANCEL: 60 * 24 * 14,
         CPBB_REBUY_REBUILD: 17,
       },
