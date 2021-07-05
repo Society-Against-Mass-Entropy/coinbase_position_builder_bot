3e2e3f27bd5106bd615b48a6db551afe444286ef
[._.]/ Adam Eivy <70015+atomantic@users.noreply.github.com>
2021-07-05 09:02:31 -0700

    backtests for 50% and 100% APY targets from 2016-2021 start points

--- test/backtest.test.js
+++ test/backtest.test.js
@@ -3,8 +3,9 @@ process.env.CPBB_TICKER = 'TEST';
 process.env.CPBB_CURRENCY = 'USD';
 process.env.CPBB_VOL = 100;
 process.env.CPBB_FREQ = '1 1 1 1 1';
-process.env.CPBB_APY = 25;
+// process.env.CPBB_APY = 100;
 process.env.CPBB_TEST = true;
+// process.env.CPBB_TEST_START = `2016-01-01`;
 // process.env.CPBB_REBUY_MAX = 50;
 // process.env.CPBB_REBUY = '.0001@2,.0002@4,.0003@6,.0004@8,.0005@10';
 // // process.env.CPBB_REBUY_SIZE = '.0001,.0002,.0003,.0004,.0005';
@@ -18,13 +19,18 @@ process.env.CPBB_TEST = true;
 // process.env.CPBB_RESELL_REBUILD = 6;
 const verbose = process.env.VERBOSE_TEST === 'true';
 
+const fs = require('fs');
 // this must be included before loading anything else so we can capture console.log output
 const log = require('./lib/log.backtest');
 const deleteOutputFiles = require('./lib/delete.output.files');
 
 const priceHistory = require('../data/rates.daily.BTC-USD.json').daily;
+const startDate = new Date(process.env.CPBB_TEST_START);
+const testHistory = priceHistory.filter(
+  h => new Date(h[0] * 1000) >= startDate
+);
 
-process.env.FIRST_LOG_DATE = new Date(priceHistory[0][0] * 1000).toISOString();
+process.env.FIRST_LOG_DATE = new Date(testHistory[0][0] * 1000).toISOString();
 require('./nock/delete.order');
 require('./nock/get.accounts');
 require('./nock/get.order');
@@ -43,10 +49,12 @@ const testMemory = require('./lib/test.memory');
 // const memory = require('../lib/memory');
 
 // dates to begin test sequence
-let currentDate = new Date(priceHistory[0][0] * 1000);
+let currentDate = new Date(testHistory[0][0] * 1000);
 // a list of filenames in ./data/output.${name}.log that group
 // chunks of expected stdout from the app
-const logExpectations = ['backtest'];
+const logExpectations = [
+  `backtest_${process.env.CPBB_TEST_START}_${process.env.CPBB_APY}`,
+];
 
 describe('Backtest Engine', () => {
   // as we load up the log expectations,
@@ -62,8 +70,8 @@ describe('Backtest Engine', () => {
     await sleep(2000);
 
     // mock market conditions and actions as if the cronjob triggered
-    for (let i = 0; i < priceHistory.length; i++) {
-      let [time, low, high, open, close] = priceHistory[i];
+    for (let i = 0; i < testHistory.length; i++) {
+      let [time, low, high, open, close] = testHistory[i];
       // if (i % 24) {
       // checks limits and runs an action event
       testMemory.open = open;
@@ -81,6 +89,14 @@ describe('Backtest Engine', () => {
   logExpectations.forEach(name => {
     test(`Log Output: ${name}`, () => {
       const expectedLog = getLog(name);
+      if (!expectedLog.length) {
+        // no existing log file, creating snapshot
+        expectedLog.push(...log.out);
+        fs.writeFileSync(
+          `${__dirname}/data/output.${name}.log`,
+          expectedLog.join('\n')
+        );
+      }
       expectedLog.forEach(l => {
         if (!l) return;
         logIndex++;
--- test/lib/delete.output.files.js
+++ test/lib/delete.output.files.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 module.exports = () => {
   const historyFile = `${__dirname}/../../data/history.${process.env.CPBB_TICKER}-${process.env.CPBB_CURRENCY}.sandbox.tsv`;
   const ordersFile = `${__dirname}/../../data/maker.orders.${process.env.CPBB_TICKER}-${process.env.CPBB_CURRENCY}.sandbox.json`;
-  if (fs.existsSync(historyFile)) fs.unlinkSync(historyFile);
+  if (fs.existsSync(historyFile))
+    fs.renameSync(historyFile, historyFile + '.archive.tsv');
   if (fs.existsSync(ordersFile)) fs.unlinkSync(ordersFile);
 };
