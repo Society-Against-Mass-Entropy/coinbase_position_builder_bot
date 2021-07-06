d4b6147cdda69ea5cee8063f84583a58cd6fcd6f
Jason Edison <jason.x.edison@gmail.com>
2021-07-06 12:50:19 -0700

    merge develop

--- run.mine.config.js
+++ run.mine.config.js
@@ -14,17 +14,17 @@ module.exports = {
         CPBB_APIPASS: apiKeys.CPBB_APIPASS,
         CPBB_APIKEY: apiKeys.CPBB_APIKEY,
         CPBB_APISEC: apiKeys.CPBB_APISEC,
-        CPBB_FREQ: '02 */6 * * *',
+        CPBB_FREQ: '02 6 * * *',
         CPBB_TICKER: 'BTC',
         CPBB_CURRENCY: 'USD',
-        CPBB_VOL: 100,
-        CPBB_APY: 150,
+        CPBB_VOL: 400,
+        CPBB_APY: 100,
         // max $ spend on limit rebuys
-        CPBB_REBUY_MAX: 50,
+        // CPBB_REBUY_MAX: 50,
         // minimum order is .0001 BTC ($5 at $50K)
         // rebuy logic will place up orders at this size until CPBB_REBUY_MAX is reached
-        CPBB_REBUY:
-          '.0001@4,.0002@5,.0003@6,.0004@7,.0005@8,.0006@9,.0007@10,.0008@11,.0009@12,.001@13,.0015@14,.002@15,.004@20,.008@25,.016@30,.032@35,.064@40,.128@45,.256@50,.512@55,1@60,2@65,4@70,8@75,16@80,32@85,64@90,128@95,200@96,200@97,200@98,200@99',
+        // CPBB_REBUY:
+        // '.0001@3,.0005@6,.001@9,.005@12,.01@15,.05@20,.1@25,.5@30,1@50',
         // when should we cancel limit orders?
         // default behavior is on the next action point (if they didn't fill)
         // if CPBB_REBUY_CANCEL is set, this is a number of minutes after the limit order
@@ -37,11 +37,11 @@ module.exports = {
         // NOTE: if you use this setting, it is recommended that you set it higher than
         // the number of items in your CPBB_REBUY config so it doesn't excessively rebuild
         // the same orders over and over
-        CPBB_REBUY_REBUILD: 35,
-        // sell up to $100 worth of asset
-        CPBB_RESELL_MAX: 100,
+        // CPBB_REBUY_REBUILD: 35,
+        // sell up to $50 worth of asset
+        // CPBB_RESELL_MAX: 50,
         // sell up to 1 units of asset @ +10% pump
-        CPBB_RESELL: '1@10',
+        // CPBB_RESELL: '1@10',
         // no rebuild, auto cancel before next run
       },
     },
@@ -55,11 +55,11 @@ module.exports = {
         CPBB_APIPASS: apiKeys.CPBB_APIPASS,
         CPBB_APIKEY: apiKeys.CPBB_APIKEY,
         CPBB_APISEC: apiKeys.CPBB_APISEC,
-        CPBB_FREQ: '12 */6 * * *',
+        CPBB_FREQ: '12 6 * * *',
         CPBB_TICKER: 'ETH',
         CPBB_CURRENCY: 'USD',
-        CPBB_VOL: 25,
-        CPBB_APY: 25,
+        CPBB_VOL: 100,
+        CPBB_APY: 20,
         // max $ spend on limit rebuys
         // CPBB_REBUY_MAX: 10,
         // // minimum order is .001 ETH ($5 at $5K)
@@ -68,11 +68,8 @@ module.exports = {
         //   '.001@4,.002@5,.003@6,.004@8,.005@10,.01@15,.02@20,.04@25,.08@30,.16@35,.32@40,.64@50,1.28@60,2.56@70,5.12@80,10.24@90',
         // CPBB_REBUY_CANCEL: 60 * 24 * 14,
         // CPBB_REBUY_REBUILD: 35,
-        // sell up to $5 worth of asset
         CPBB_RESELL_MAX: 25,
-        // sell up to 10 units of asset @ +5% pump
         CPBB_RESELL: '10@5',
-        // no rebuild, auto cancel before next run
       },
     },
     {
@@ -84,11 +81,11 @@ module.exports = {
         CPBB_APIPASS: apiKeys.CPBB_APIPASS,
         CPBB_APIKEY: apiKeys.CPBB_APIKEY,
         CPBB_APISEC: apiKeys.CPBB_APISEC,
-        CPBB_FREQ: '28 */6 * * *',
+        CPBB_FREQ: '28 6 * * *',
         CPBB_TICKER: 'LTC',
         CPBB_CURRENCY: 'USD',
-        CPBB_VOL: 20,
-        CPBB_APY: 25,
+        CPBB_VOL: 50,
+        CPBB_APY: 15,
         // max $ spend on limit rebuys
         // CPBB_REBUY_MAX: 10,
         // // minimum order is in LTC (.01, which is $5 at $500)
@@ -98,7 +95,7 @@ module.exports = {
         // CPBB_REBUY_CANCEL: 60 * 24 * 14,
         // CPBB_REBUY_REBUILD: 20,
         // sell up to $5 worth of asset
-        CPBB_RESELL_MAX: 20,
+        CPBB_RESELL_MAX: 25,
         // sell up to 10 units of asset @ +5% pump
         CPBB_RESELL: '10@5',
         // no rebuild, auto cancel before next run
@@ -113,13 +110,13 @@ module.exports = {
         CPBB_APIPASS: apiKeys.CPBB_APIPASS,
         CPBB_APIKEY: apiKeys.CPBB_APIKEY,
         CPBB_APISEC: apiKeys.CPBB_APISEC,
-        CPBB_FREQ: '35 */6 * * *',
+        CPBB_FREQ: '35 6 * * *',
         CPBB_TICKER: 'DASH',
         CPBB_CURRENCY: 'USD',
-        CPBB_VOL: 5,
+        CPBB_VOL: 20,
         CPBB_APY: 10,
         // sell up to $5 worth of DASH
-        CPBB_RESELL_MAX: 5,
+        CPBB_RESELL_MAX: 20,
         // sell up to 100 DASH @ +5% pump
         CPBB_RESELL: '100@5',
         // no rebuild, auto cancel before next run
@@ -134,11 +131,11 @@ module.exports = {
         CPBB_APIPASS: apiKeys.CPBB_APIPASS,
         CPBB_APIKEY: apiKeys.CPBB_APIKEY,
         CPBB_APISEC: apiKeys.CPBB_APISEC,
-        CPBB_FREQ: '46 */12 * * *',
+        CPBB_FREQ: '46 6 * * *',
         CPBB_TICKER: 'XTZ',
         CPBB_CURRENCY: 'USD',
-        CPBB_VOL: 25,
-        CPBB_APY: 100,
+        CPBB_VOL: 50,
+        CPBB_APY: 50,
         // max $ spend on limit rebuys
         // CPBB_REBUY_MAX: 10,
         // // minimum order is 1 XTZ, with a max precision of .01 XTZ
@@ -147,7 +144,7 @@ module.exports = {
         // CPBB_REBUY_CANCEL: 60 * 24 * 14,
         // CPBB_REBUY_REBUILD: 25,
         // sell up to $5 worth of asset
-        CPBB_RESELL_MAX: 25,
+        CPBB_RESELL_MAX: 50,
         // sell up to 100 units of asset @ +5% pump
         CPBB_RESELL: '100@5',
         // no rebuild, auto cancel before next run
@@ -162,11 +159,11 @@ module.exports = {
         CPBB_APIPASS: apiKeys.CPBB_APIPASS,
         CPBB_APIKEY: apiKeys.CPBB_APIKEY,
         CPBB_APISEC: apiKeys.CPBB_APISEC,
-        CPBB_FREQ: '53 5,17 * * *',
+        CPBB_FREQ: '53 6 * * *',
         CPBB_TICKER: 'DOGE',
         CPBB_CURRENCY: 'USD',
         CPBB_VOL: 10,
-        CPBB_APY: 20,
+        CPBB_APY: 10,
         // sell up to $5 worth of asset
         CPBB_RESELL_MAX: 10,
         // sell up to 100 units of asset @ +5% pump
